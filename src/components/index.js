597269c83bdfc58060f44154769e6cdfcaaa2f42
Hesam Bayat <hesam.bayat@gmail.com>
2021-06-19 15:29:18 +0400

    Add el-logo

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import Checkbox from './el-checkbox'
 import Grid from './el-grid'
+import Logo from './el-logo'
 import Panel from './el-panel'
 import Plugin from './el-plugin'
 
 export {
   Checkbox,
   Grid,
+  Logo,
   Panel,
   Plugin,
 }
