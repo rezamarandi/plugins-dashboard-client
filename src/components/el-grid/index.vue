3b40d9e3cf82e2d4ff7f1fae846d72963df61d52
Hesam Bayat <hesam.bayat@gmail.com>
2021-06-19 14:28:49 +0400

    Add el-grid component

---
+++ src/components/el-grid/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grid"><slot /></div>
+</template>
+
+<script>
+export default {
+  name: 'el-grid',
+  props: ['col']
+}
+</script>
+
+<style>
+  .grid {
+    display: grid;
+    column-gap: 30px;
+    row-gap: 30px;
+  }
+
+  @media screen and (min-width: 768px){
+    .grid {
+      grid-template-columns: 1fr 1fr;
+    }    
+  }
+
+  @media screen and (min-width: 1200px){
+    .grid {
+      grid-template-columns: 1fr 1fr 1fr;
+    }    
+  }
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Checkbox from './el-checkbox'
+import Grid from './el-grid'
 import Panel from './el-panel'
 import Plugin from './el-plugin'
 
 export {
   Checkbox,
+  Grid,
   Panel,
   Plugin,
 }
